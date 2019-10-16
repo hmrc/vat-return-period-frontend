3db66838c8d34e49168d4c67593e6c3bcf82a835
JoshuaGomersallHMRC <53816372+JoshuaGomersallHMRC@users.noreply.github.com>
2019-10-16 14:30:55 +0100

    BTAT-6955 removed routes and controller after rebase

--- app/controllers/ChangeClientController.scala
+++ app/controllers/ChangeClientController.scala
@@ -31,7 +31,7 @@ class ChangeClientController @Inject()(val authenticate: AuthPredicate,
   def changeClient: Action[AnyContent] = authenticate.async {
     implicit user =>
       Future.successful(
-        Redirect(appConfig.agentClientLookupStartUrl(controllers.returnFrequency.routes.ChooseDatesController.show().url))
+        Redirect(appConfig.agentClientLookupStartUrl(appConfig.manageVatUrl))
           .removingFromSession(SessionKeys.CLIENT_VRN, SessionKeys.NEW_RETURN_FREQUENCY, SessionKeys.CURRENT_RETURN_FREQUENCY, SessionKeys.ANNUAL_ACCOUNTING_PENDING)
       )
   }
