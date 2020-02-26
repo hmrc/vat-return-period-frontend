c40779a3cb103a9ac24cc97da6a253c2e642522d
Sam Raspin <14580184+sraspin@users.noreply.github.com>
2020-02-26 17:17:41 +0000

    [BTAT-7560] - removed redundant tests

--- test/views/returnFrequency/ChangeReturnFrequencyConfirmationViewSpec.scala
+++ test/views/returnFrequency/ChangeReturnFrequencyConfirmationViewSpec.scala
@@ -75,77 +75,8 @@ class ChangeReturnFrequencyConfirmationViewSpec extends ViewBaseSpec {
         lazy val view = views.html.returnFrequency.change_return_frequency_confirmation(contactPref = Some("DIGITAL"))(user, messages, mockAppConfig)
         lazy implicit val document: Document = Jsoup.parse(view.body)
 
-        s"have the correct document title of '${viewMessages.ReceivedPage.title}'" in {
-          document.title shouldBe viewMessages.ReceivedPage.title
-        }
-
-        s"have a correct page heading of '${viewMessages.ReceivedPage.heading}'" in {
-          elementText("#page-heading") shouldBe viewMessages.ReceivedPage.heading
-        }
-
-        s"have the correct h2 '${viewMessages.ReceivedPage.h2}'" in {
-          elementText("h2") shouldBe viewMessages.ReceivedPage.h2
-        }
-
         s"have the correct p1 of '${viewMessages.ReceivedPage.p1}'" in {
           paragraph(1) shouldBe viewMessages.ReceivedPage.digitalPref
-        }
-
-        s"have the correct p2 of '${viewMessages.ReceivedPage.p2}'" in {
-          paragraph(2) shouldBe viewMessages.ReceivedPage.contactDetails
-        }
-
-        "not have a link to change client" in {
-          elementExtinct("#change-client-text")
-        }
-
-        "have the correct finish button" which {
-
-          s"has the text '${viewMessages.finish}'" in {
-            elementText("#finish") shouldBe viewMessages.finish
-          }
-
-          "has link back to customer details page" in {
-            element("#finish").attr("href") shouldBe mockAppConfig.manageVatUrl
-          }
-        }
-      }
-
-      lazy val view = views.html.returnFrequency.change_return_frequency_confirmation(contactPref = Some("DIGITAL"))(user, messages, mockAppConfig)
-      lazy implicit val document: Document = Jsoup.parse(view.body)
-
-      s"have the correct document title of '${viewMessages.ReceivedPage.title}'" in {
-        document.title shouldBe viewMessages.ReceivedPage.title
-      }
-
-      s"have a correct page heading of '${viewMessages.ReceivedPage.heading}'" in {
-        elementText("#page-heading") shouldBe viewMessages.ReceivedPage.heading
-      }
-
-      s"have the correct h2 '${viewMessages.ReceivedPage.h2}'" in {
-        elementText("h2") shouldBe viewMessages.ReceivedPage.h2
-      }
-
-      s"have the correct p1 of '${viewMessages.ReceivedPage.p1}'" in {
-        paragraph(1) shouldBe viewMessages.ReceivedPage.digitalPref
-      }
-
-      s"have the correct p2 of '${viewMessages.ReceivedPage.p2}'" in {
-        paragraph(2) shouldBe viewMessages.ReceivedPage.contactDetails
-      }
-
-      "not have a link to change client" in {
-        elementExtinct("#change-client-text")
-      }
-
-      "have the correct finish button" which {
-
-        s"has the text '${viewMessages.finish}'" in {
-          elementText("#finish") shouldBe viewMessages.finish
-        }
-
-        "has link back to customer details page" in {
-          element("#finish").attr("href") shouldBe mockAppConfig.manageVatUrl
         }
       }
     }
